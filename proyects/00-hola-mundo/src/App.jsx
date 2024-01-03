b02c85df696106064ba41c5bcce668817c0c3f1c
Cristian95vr <cristianvegareyes95@gmail.com>
2024-01-03 19:50:19 -0300

    First class

---
+++ proyects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'Cristiano',
+    name: 'Cristiano',
+    isFollowing: false
+  },
+  {
+    userName: 'KimKardashian',
+    name: 'Kim Kardashian',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  }
+]
+
+export function App () {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
+}
