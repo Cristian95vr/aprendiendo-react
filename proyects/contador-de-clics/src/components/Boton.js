a72029d5aa70de6294b28a87c4c73e5394772e11
Cristian95vr <cristianvegareyes95@gmail.com>
2024-02-02 16:21:35 -0300

    fcc-useState

---
+++ proyects/contador-de-clics/src/components/Boton.js
@@ -0,0 +1,11 @@
+import '../stylesheets/Boton.css';
+
+function Boton({texto, esBotonDeClic, manejarClic}) {
+    return (
+        <button className={esBotonDeClic ? 'boton-click': 'boton-reiniciar' } onClick={manejarClic}>
+            {texto}
+        </button>
+    )
+}
+
+export default Boton;
