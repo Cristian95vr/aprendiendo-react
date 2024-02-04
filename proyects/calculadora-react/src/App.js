0a29e6cc6ac68e33c4211d73ea0b4270dd817867
Cristian95vr <cristianvegareyes95@gmail.com>
2024-02-04 03:21:30 -0300

    Estructura de la calculadora

--- proyects/calculadora-react/src/App.js
+++ proyects/calculadora-react/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import freecodecampLogo from './imagenes/freecodecamp-logo.png'
 
 function App() {
   return (
     <div className='App'>
-      <div className='container'>
-
+      <div className='freecodecamp-logo-container'>
+        <img src={freecodecampLogo} className='freecodecamp-logo' alt='freecodecamp logo' />
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div> 
+        <div className='fila'></div>
       </div>
     </div>
   );
