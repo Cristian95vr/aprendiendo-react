d3bc04cc8be4ab37110658f6ec4f0735354f57d4
Cristian95vr <cristianvegareyes95@gmail.com>
2024-02-05 01:48:55 -0300

    diseño de la calculadora

--- proyects/calculadora-react/src/App.js
+++ proyects/calculadora-react/src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import freecodecampLogo from './imagenes/freecodecamp-logo.png'
+import Boton from './components/Boton';
+import Pantalla from './components/Pantalla';
+import BotonClear from './components/BotonClear';
+
 
 function App() {
   return (
@@ -8,11 +12,34 @@ function App() {
         <img src={freecodecampLogo} className='freecodecamp-logo' alt='freecodecamp logo' />
       </div>
       <div className='contenedor-calculadora'>
-        <div className='fila'></div>
-        <div className='fila'></div>
-        <div className='fila'></div>
-        <div className='fila'></div> 
-        <div className='fila'></div>
+        <Pantalla />
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+          </div> 
+        <div className='fila'>
+          <BotonClear>Clear</BotonClear>
+        </div>
       </div>
     </div>
   );
