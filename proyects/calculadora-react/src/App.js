9610b0949ed25cd8d5086f28790439c7a88c4512
Cristian95vr <cristianvegareyes95@gmail.com>
2024-02-07 00:52:03 -0300

    calculadora funcionando

--- proyects/calculadora-react/src/App.js
+++ proyects/calculadora-react/src/App.js
@@ -3,42 +3,59 @@ import freecodecampLogo from './imagenes/freecodecamp-logo.png'
 import Boton from './components/Boton';
 import Pantalla from './components/Pantalla';
 import BotonClear from './components/BotonClear';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = valor =>{
+    setInput(input + valor);
+  };
+
+  const calculadorResultado = () =>{
+    if(input){
+      setInput(evaluate(input));
+  } else {
+      alert("Por favor ingrese valores para realizar los calculos")
+  }
+  };
+
   return (
     <div className='App'>
       <div className='freecodecamp-logo-container'>
         <img src={freecodecampLogo} className='freecodecamp-logo' alt='freecodecamp logo' />
       </div>
       <div className='contenedor-calculadora'>
-        <Pantalla />
+        <Pantalla input={input} />
         <div className='fila'>
-          <Boton>1</Boton>
-          <Boton>2</Boton>
-          <Boton>3</Boton>
-          <Boton>+</Boton>
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
         </div>
         <div className='fila'>
-          <Boton>4</Boton>
-          <Boton>5</Boton>
-          <Boton>6</Boton>
-          <Boton>-</Boton>
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
         </div>
         <div className='fila'>
-          <Boton>7</Boton>
-          <Boton>8</Boton>
-          <Boton>9</Boton>
-          <Boton>*</Boton>
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
         </div>
         <div className='fila'>
-          <Boton>=</Boton>
-          <Boton>0</Boton>
-          <Boton>.</Boton>
-          <Boton>/</Boton>
+          <Boton manejarClic={calculadorResultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
           </div> 
         <div className='fila'>
-          <BotonClear>Clear</BotonClear>
+          <BotonClear manejarClear={() => setInput('')}>Clear</BotonClear>
         </div>
       </div>
     </div>
--- proyects/calculadora-react/src/components/Boton.js
+++ proyects/calculadora-react/src/components/Boton.js
@@ -4,11 +4,11 @@ import '../hojas-de-estilo/Boton.css';
 function Boton(props){
 
     const esOperador= valor =>{
-        return isNaN(valor) && (valor != '.') && (valor != '=');
+        return isNaN(valor) && (valor !== '.') && (valor !== '=');
     }
 
     return (
-        <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}>
+        <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`} onClick={()=> props.manejarClic(props.children)}>
             {props.children}
         </div>
     )
